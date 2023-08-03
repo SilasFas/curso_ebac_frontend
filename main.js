0eb767687f0672f21d1b6061ebe14fd8143eed01
Silas <silasfas@hotmail.com>
2023-08-03 00:21:09 -0300

    fix: bug consertado - era preciso converte a string para number

--- main.js
+++ main.js
@@ -4,14 +4,19 @@ const inputButton = document.querySelector('#inputBtn')
 const mensagem = document.getElementById('mensagem')
 
 inputButton.addEventListener('click', ((event) => {
-    // bloqueia a envio do formulário e o recarregamento da página 
+    // bloqueia o envio do formulário e o recarregamento da página 
     event.preventDefault()
 
     //Verifica se os campos estão vazios
     if (numeroB.value && numeroA.value) {
 
+        // Converte os valores para números
+        const valorA = Number(numeroA.value)
+        const valorB = Number(numeroB.value)
+
         //Verifica se o valor B é maior do que o A
-        if (numeroB.value > numeroA.value) {
+        if (valorB > valorA) {
+
             // Se o valor B for maior do que o A 
             mensagem.innerText = 'POSITIVO - Formulário válido pois o número B é maior do que o número A'
             mensagem.style.color = 'green'
@@ -21,6 +26,7 @@ inputButton.addEventListener('click', ((event) => {
             numeroB.value = ''
 
         } else {
+
             // Se o valor A for maior do que o B 
             mensagem.innerText = 'NEGATIVO Formulário inválido: pois o número B precisa ser maior do que o A'
             mensagem.style.color = 'red'
@@ -31,6 +37,7 @@ inputButton.addEventListener('click', ((event) => {
         }
 
     } else {
+
         // Se um dos campos ou os dois campos estiverem vazios
         mensagem.innerText = 'Os campos estão vazios'
         mensagem.style.color = 'red'
