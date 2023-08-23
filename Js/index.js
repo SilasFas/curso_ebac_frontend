0bb1f3c95bd98222152092116359bc8254b777b8
Silas <silasfas@hotmail.com>
2023-08-23 17:45:48 -0300

    coloquei botão e apos clicar valores ficam em branco

--- Js/index.js
+++ Js/index.js
@@ -5,4 +5,8 @@ $(document).ready(function () {
     $('#tel').mask('(00) 00000-0000')
     $('#cpf').mask('000.000.000-00')
     $('#cep').mask('00000-000')
+
+    $('button').on('click', function () {
+        $('.valor').val('')
+    })
 })
